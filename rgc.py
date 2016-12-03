08eee6ee8c74705e52cbdc13dbbc0ffdd1c7f4df
luzijia <luzijia@gmail.com>
2016-12-04 01:07:24 +0800

    rgc v3.0 init

---
+++ rgc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python -u
+# -*- coding: UTF-8 -*-
+"""
+Usage: rgc [options]
+
+Options:
+    --version             show program's version number and exit
+    -h, --help          show this help message and exit
+    -f FILE, --filename=FILE
+"""
+
+import os
+from rigger_code.core import *
+from optparse import OptionParser
+
+def main():
+    usage = "usage: rgc [options]"
+    parser = OptionParser(usage=usage,version="%prog 2.0")
+    parser.add_option("-f","--filename",metavar="FILE", help="the configuration file,default /ett/rgct.ini")
+    (options, args) = parser.parse_args()
+    filename =  options.filename
+    if(filename==None):
+        print __doc__
+    else:
+        if os.path.exists(filename):
+            core.init(filename)
+            core.runmodel("User")
+        else:
+            print "%s not exists"%(filename)
+
+if __name__ == "__main__":
+    main()
